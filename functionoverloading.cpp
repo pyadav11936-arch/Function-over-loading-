ec3ac714d02f3a58bcad30d5e1a41b29923fcdd7
Priyanshu Yadav <pyadav11936@gmail.com>
2026-08-28 21:24:34 +0530

    Add files via upload

---
+++ functionoverloading.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void fun(){
+    cout<<"hii"<<endl;
+}
+void fun(int x){
+    cout<<"priyanshu"<<endl;
+}
+void fun(int x, int y){   
+    cout<<"how are you"<<endl;
+}
+int main(){
+    fun();
+}
